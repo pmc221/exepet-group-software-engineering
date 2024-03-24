e1d706c60b7f5f80eae911cdf1b6952d9f446793
pmc221 <124456033+pmc221@users.noreply.github.com>
2024-03-24 16:49:33 +0000

    Add files via upload

---
+++ EXEplorer/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from . import views
+from .views import reset_user_profile
+
+
+urlpatterns = [
+    path("", views.home_view, name="home"),
+    path('health', views.health, name='health'),
+    path('game/', views.game_view, name='game'),
+    path('handle-scanned-number/', views.handle_scanned_number, name='handle_scanned_number'),
+    path('shop/', views.shop_view, name='shop'),
+    path('tree/', views.tree_view, name='tree'),
+    path('qrcode/', views.qrcode_view, name='qrcode'),
+    path('reset_user_profile', reset_user_profile, name='reset_user_profile'),
+    path('api/get_cherries_value/', views.get_cherries_value, name='get_cherries_value'),
+    path('api/update_cherries_value/', views.update_cherries_value, name='update_cherries_value'),
+    path('api/get_datetime_value/', views.get_datetime_value, name='get_datetime_value'),
+    path('api/update_datetime_value/', views.update_datetime_value, name='update_datetime_value'),
+
+
+
+
+    path('api/get_carbon_footprint_value/', views.get_carbon_footprint_value, name='get_carbon_footprint_value'),
+    path('api/increment_carbon_footprint/', views.increment_carbon_footprint, name='increment_carbon_footprint'),
+    path('api/decrement_carbon_footprint/', views.decrement_carbon_footprint, name='decrement_carbon_footprint'),
+    path('start-pet-walk/', views.petDisappear, name='start-pet-walk'),
+    path('pet-check', views.petCheck, name = 'pet-check'),
+    path('scan/', views.scan_view, name='scan'),
+    path('add-cherries', views.addCherries, name='add-cherries'),
+]
